b4cae28fff58aef9f5118c30773aeca75f01d4a8
rory <rhrg500@gmail.com>
2017-02-16 12:02:19 +0000

    replaces sing method with verse

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
 
-  def initialize(number_of_bottles)
+  def initialize
 
   end
 
-  def sing
-    puts "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall."
+  def verse(number_of_bottles)
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 
 end
