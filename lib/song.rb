81c7d3657a96e695b87b41fea2fe8f63dd9b88a8
rory <rhrg500@gmail.com>
2017-02-16 14:08:18 +0000

    second argument in verses now equals first argument as default

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+
+
 class Song
 
   def initialize
@@ -8,7 +10,7 @@ class Song
     "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\nTake one down and pass it around, #{number_of_bottles-1} bottles of beer on the wall.\n"
   end
 
-  def verses(initial_bottles, final_bottles)
+  def verses(initial_bottles, final_bottles = initial_bottles)
     output = ""
     for number_of_bottles in (initial_bottles).downto(final_bottles)
       output += verse(number_of_bottles) + "\n"
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -9,7 +9,7 @@ describe Song do
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
-    expect(song.verse(0)).to eq(expected)
+    expect(song.verses(0)).to eq(expected)
   end
 
   it "returns the verse for 99 bottles" do
@@ -17,7 +17,7 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
     VERSE
-    expect(song.verse(99)).to eq(expected)
+    expect(song.verses(99)).to eq(expected)
   end
 
   it "returns the verse for 98 bottles" do
@@ -25,7 +25,7 @@ Take one down and pass it around, 98 bottles of beer on the wall.
 98 bottles of beer on the wall, 98 bottles of beer.
 Take one down and pass it around, 97 bottles of beer on the wall.
     VERSE
-    expect(song.verse(98)).to eq(expected)
+    expect(song.verses(98)).to eq(expected)
   end
 
   it "sings 2 consecutive verses" do
@@ -52,6 +52,7 @@ Take one down and pass it around, 96 bottles of beer on the wall.
     VERSE
     expect(song.verses(99,97)).to eq(expected)
   end
+
 
 
 
