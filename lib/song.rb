8846fc80ed6c832effa09743a4ddc508d1854ec7
Chris Cooper <cjcooper252@gmail.com>
2017-02-16 12:08:06 +0000

    returns the first verse for 99 bottles

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   end
 
   def verse(number_of_bottles)
-    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
   end
 
 end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -4,10 +4,18 @@ describe Song do
 
   subject(:song) { described_class.new }
 
-  it "returns the final verse when there are no bottles left" do
+  xit "returns the final verse when there are no bottles left" do
     expected = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
+    VERSE
+    expect(song.verse(0)).to eq(expected)
+  end
+
+  it "returns the first verse" do
+    expected = <<-VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
     VERSE
     expect(song.verse(0)).to eq(expected)
   end
