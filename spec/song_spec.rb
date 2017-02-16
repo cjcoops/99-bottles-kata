72c4099aa5221387133dfb200e6788428bbe58c1
rory <rhrg500@gmail.com>
2017-02-16 12:03:02 +0000

    changes final verse test to use expected using <<- to deal with multi-line string

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -2,12 +2,14 @@ require 'song'
 
 describe Song do
 
+  subject(:song) { described_class.new }
+
   it "returns the final verse when there are no bottles left" do
-    song = Song.new(0)
-    final_verse = "No more bottles of beer on the wall, no more bottles of beer.
-    Go to the store and buy some more, 99 bottles of beer on the wall."
-    expect{print(final_verse)}.to output.to_stdout
-    song.sing
+    expected = <<-VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+    VERSE
+    expect(song.verse(0)).to eq(expected)
   end
 
 end
